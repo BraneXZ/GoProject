c4d9354a9b850e79c864259ee061f310f536297e
Washington Shiaohongtu <washingtonshiao@gmail.com>
2019-12-26 13:29:12 -0600

    Move init

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -1,7 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+import copy
+from dlgo.gotypes import Player
 
-This is a temporary script file.
-"""
 
+class Move():
+    """
+    Any action a player can play on a turn
+    is_play, is_pass, or is_resign 
+    will be set
+    """
+    def __init__(self, point=None, is_pass=False, is_resign=False):
+        assert (point is not None) ^ is_pass ^ is_resign
+        self.point = point
+        self.is_play = (self.point is not None)
+        self.is_pass = is_pass
+        self.is_resign = is_resign
+    
+    @classmethod
+    def play(cls, point):
+        """
+        This move places a stone on the board
+        """
+        return Move(point=point)
+
+    @classmethod
+    def pass_turn(cls):
+        """
+        This move passes
+        """
+        return Move(is_pass=True)
+
+    @classmethod
+    def resign(cls):
+        """
+        This move resigns the current game
+        """
+        return Move(is_resign=True)
