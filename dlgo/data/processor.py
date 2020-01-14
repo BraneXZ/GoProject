da4aab81c8e8fe91bf1229ee4a5e1fdf84490c71
Washington Shiaohongtu <washingtonshiao@gmail.com>
2020-01-14 16:51:45 -0600

    replay using sgf_game class

---
+++ dlgo/data/processor.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 16:51:06 2020
+
+@author: fshiaohongtu
+"""
+
---
+++ replay.py
@@ -0,0 +1,21 @@
+from dlgo.goboard import GameState, Move
+from dlgo.gotypes import Point
+from dlgo.utils import print_board
+from dlgo.gosgf.sgf import Sgf_game
+
+sgf_content = "(;GM[1]FF[4]SZ[9];B[ee];W[ef];B[ff];W[df];B[fe];W[fc];B[ec];W[gd];B[fb])"
+
+sgf_game = Sgf_game.from_string(sgf_content)
+
+game_state = GameState.new_game(19)
+
+for item in sgf_game.main_sequence_iter():
+    
+    color, move_tuple = item.get_move()
+    if color is not None and move_tuple is not None:
+        row, col = move_tuple
+        point = Point(row+1, col+1)
+        move = Move.play(point)
+        game_state = game_state.apply_move(move)
+        print_board(game_state.board)
+        
